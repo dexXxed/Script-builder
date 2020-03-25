b2bea1122ee62c8baa2eccf8d711bbd1d0f32201
dexxxed <denis22052000@gmail.com>
2020-03-25 14:19:25 +0200

    file checking

--- script.sh
+++ script.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+if [ -z "$1" ] # проверяем есть ли первый аргумент
   then
     echo "No argument supplied!!! Run with arguments!!"
     exit 1
 fi
+
+if test -f "$1"; # проверяем существует ли указанный файл
+then
+    echo "$1 exist"
+else
+    echo "No such file!!!!"
+    exit 1
+fi
+
 
 filename=$1 # получаем имя файла, прописанного в аргументах скрипта
 output_line=$(grep "// Output: " $1) # считываем первую строку файла в котором храниться "Output: xxx.out"
@@ -22,15 +31,15 @@ TMPDIR=$(mktemp -d build-$1.XXXXXX) # создаем временную дире
 trap "exit 1"           HUP INT PIPE QUIT TERM # задаем сигнал "exit 1" при любой из перечисленных ошибок
 trap 'rm -rf "$TMPDIR"' EXIT # удаляем директорию при выходе из скрипта и при неожиданной остановке
 
-cd "$TMPDIR"
+cd "$TMPDIR" # заходим во временную директорию
 gcc ../$1 -o ./$out_file_name # собираем используя gcc во временной директории 
 
-echo "pwd: $PWD" # проверяем, где мы находимся (во временной директории)
+# echo "pwd: $PWD" # проверяем, где мы находимся (во временной директории)
 
 if [[ $? -ne 0 ]]; then # проверяем все ли ок с gcc компиляцией
     echo "Compilation error!!!"
     exit 1
 fi
 
-cd ..
+cd .. # переходим на уровень нахождения скрипта
 mv ./$TMPDIR/$out_file_name . # перемещаем собранный бинарник на уровень программы
